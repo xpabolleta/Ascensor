348b7588e7c3372709cf6a2ac2c538907194c54c
Xabi <xpabolleta@gmail.com>
2024-04-29 17:59:03 +0200

    [MOD] script.js file modified Deleted vars with redundant infromation Box array position updates

--- script.js
+++ script.js
@@ -9,17 +9,13 @@ const PISO3 = 3;
 
 const BOTON_CREAR = document.getElementById("crear_caja");
 const FORMULARIO = document.querySelector("form");
-/*const BOTON_B = document.getElementById("boton_pisoB");
+const BOTON_B = document.getElementById("boton_pisoB");
 const BOTON_1 = document.getElementById("boton_piso1");
 const BOTON_2 = document.getElementById("boton_piso2");
-const BOTON_3 = document.getElementById("boton_piso3");*/
+const BOTON_3 = document.getElementById("boton_piso3");
 const ALMACEN = document.getElementById("almacen");
 
-let ascensor = {id : ASCENSOR, posicion : PISOB, estado : 0, pila : Array(), carga : Array()}
-let pisoB = {id : PISOB, carga : Array()}
-let piso1 = {id : PISO1, carga : Array()}
-let piso2 = {id : PISO2, carga : Array()}
-let piso3 = {id : PISO3, carga : Array()}
+let ascensor = {posicion : PISOB, estado : 0, pila : Array()}
 let botones = {botonB : 0, boton1 : 0, boton2 : 0, boton3 : 0,}
 let cajas = Array();
 let identificador = 0;
@@ -56,7 +52,7 @@ function CrearCaja(event){
     let ancho = document.forms["create"]["ancho"].value;
     let peso = document.forms["create"]["peso"].value;
     let dimension = Array(ancho,largo,peso);
-    let caja = new Caja(identificador,dimension,PISOB);
+    let caja = new Caja(("caja"+identificador),dimension,PISOB);
     let nueva_caja = document.createElement("div");
     let nuevo_id = document.createTextNode(identificador);
 
@@ -68,7 +64,6 @@ function CrearCaja(event){
     }
     
     cajas[identificador] = caja;
-    pisoB["carga"].push(identificador);
     nueva_caja.appendChild(nuevo_id);
     nueva_caja.className = "caja";
     nueva_caja.id = "caja" + identificador;
@@ -106,6 +101,14 @@ function drop(event) {
         draggedElement.style.top = box_posy + "px";
         draggedElement.style.left = box_posx + "px";
         event.target.appendChild(draggedElement);
+        cajas.forEach(function (valor,indice,array){
+        
+            if(valor["id"] == draggedElement.id){
+
+                cajas[indice]["posicion"] = ASCENSOR;
+            
+            }
+        });
 
     }else if(event.target.className == "almacen"){
 
@@ -114,7 +117,15 @@ function drop(event) {
         draggedElement.style.removeProperty("top");
         draggedElement.style.removeProperty("left");
         event.target.appendChild(draggedElement);
+        
+        cajas.forEach(function (valor,indice,array){
+        
+            if(valor["id"] == draggedElement.id){
 
+                cajas[indice]["posicion"] = PISOB;
+            
+            }
+        });
     }
     
 }
